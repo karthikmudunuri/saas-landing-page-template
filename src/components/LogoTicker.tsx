4c0dd2b33b8cbe62a1aede065403cbc5d03cbcc2
Mudunuri bhaskara karthikeya varma <102793643+karthikmudunuri@users.noreply.github.com>
2024-07-24 22:58:10 +0530

    Update LogoTicker.tsx

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -20,7 +20,7 @@ export const LogoTicker = () => {
   return(
     <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
-        <h2 className="text-lg text-center text-white/70 mb-16">Trusted by world's most innovative teams</h2>
+        <h2 className="text-lg text-center text-white/70 mb-16">Trusted by world&apos;s most innovative teams</h2>
         <LogoCarousel/>
         
         
