9793bcc825f0b6603c22671e5fab835a5371b18b
Mudunuri bhaskara karthikeya varma <102793643+karthikmudunuri@users.noreply.github.com>
2024-07-25 00:04:33 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ export const Hero = () => {
     <div className="bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-[72px] sm:py-24 relative overflow-clip">
       <div className="absolute h-[375px] w-[750px] sm:w-[1536px] sm:h-[768px] lg:w-[2400px] llg:h-[800px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
     <div className="container relative">
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center -mt-10">
         <AnimatedGradientTextDemo/>
       </div>
       <div className="flex justify-center mt-8 ">
